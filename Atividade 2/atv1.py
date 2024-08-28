58aae98eb566633e33e41715c3da3b7d4afe96a2
Leandro Lucena <leandrolucena0345@gmail.com>
2024-08-28 16:39:49 -0300

    Delete Atividade 2 directory

--- Atividade 2/atv1.py
+++
@@ -1,7 +0,0 @@
-def soma(num1, num2):
-  resultado = num1 + num2
-  return resultado
-numero1 = 10
-numero2 = 5
-soma_dos_numeros = soma(numero1, numero2)
-print(f"A soma de {numero1} e {numero2} é: {soma_dos_numeros}")
